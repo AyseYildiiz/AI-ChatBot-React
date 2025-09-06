1503b8eb48f9b923af4035fb9e16904ccd9c4430
Ayse Yildiz <ayseyildizz1923@gmail.com>
2025-09-06 17:55:31 +0300

    Theme changes tooked back for now

--- src/Components/ChatBotStart.jsx
+++ src/Components/ChatBotStart.jsx
@@ -1,31 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import './ChatBotStart.css'
-import '../index.css'
+import { useState } from "react";
+import "../index.css";
+import "./ChatBotStart.css";
 
 const ChatBotStart = ({ onStartChat }) => {
-    const [darkMode, setDarkMode] = useState(false);
-    useEffect( () => {
-        if (darkMode) {
-      document.body.classList.add('dark');
-      document.body.classList.remove('light');
-    } else {
-      document.body.classList.add('light');
-      document.body.classList.remove('dark');
-    }
-  }, [darkMode]);
-
-  const handleDarkMode = () => setDarkMode(true);
-  const handleLightMode = () => setDarkMode(false);
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <div className='start-page'>
-        <button className="start-page-btn" onClick={ onStartChat }> Chat AI</button>
-        <div className="theme">
-            <i className="bx bx-sun light" onClick={handleLightMode}></i>
-            <i className="bx bx-moon dark" onClick={handleDarkMode}></i>   
-        </div>
+    <div className="start-page">
+      <button className="start-page-btn" onClick={onStartChat}>
+        {" "}
+        Chat AI{" "}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default ChatBotStart
+export default ChatBotStart;
